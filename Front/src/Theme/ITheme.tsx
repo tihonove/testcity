4f73802c7a9ab78af49b7e036c9498eaf4c418ad
Milkov Daniil <milkov.de@skbkontur.ru>
2024-08-29 13:54:25 +0500

    цвета другие

--- Front/src/Theme/ITheme.tsx
+++ Front/src/Theme/ITheme.tsx
@@ -18,7 +18,7 @@ export const normalTheme: ITheme = {
     backgroundColor1: "rgba(0,0,0,0.05)",
     primaryTextColor: "#3D3D3D",
     activeLinkColor: "#000",
-    failedTextColor: "#CB3D35",
+    failedTextColor: "rgb(169, 15, 26)",
     successTextColor: "#538A1B",
     smallTextSize: "14px",
     mutedTextColor: "rgba(0, 0, 0, 0.48)",
