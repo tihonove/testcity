cbb3f171a9c5d36086c9d65eabe98678ede41542
Milkov Daniil <milkov.de@skbkontur.ru>
2024-11-01 21:34:38 +0500

    фикс отображения имени тест кейсов

--- Front/src/Pages/JobRunPage.tsx
+++ Front/src/Pages/JobRunPage.tsx
@@ -57,7 +57,21 @@ interface TestNameProps {
 }
 
 function TestName(props: TestNameProps): React.JSX.Element {
-    const splitValue = useMemo(() => props.value.split(/[.]/), [props.value]);
+    const splitValue = useMemo(() => {
+        const parts = props.value.split("(");
+        if (parts.length > 1) {
+            const lastPart = parts.slice(1);
+            const dotParts = parts[0].split(/[.]/);
+            const prefix = dotParts.slice(0, -2).join(".");
+            const testcaseName = dotParts.slice(-2).join(".");
+            return [prefix, `${testcaseName}(${lastPart.join("(")}`];
+        }
+        
+        const dotParts = props.value.split(/[.]/);
+        const prefix = dotParts.slice(0, -2).join(".");
+        const testcaseName = dotParts.slice(-2).join(".");
+        return [prefix, testcaseName];
+    }, [props.value]);
     return (
         <>
             {splitValue.slice(-2).join(".")}
