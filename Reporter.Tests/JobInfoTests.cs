67164297f4effb0cb0efc29af21a2157f27febeb
Milkov Daniil <milkov.de@skbkontur.ru>
2024-11-05 12:07:32 +0500

    Тесты

--- Reporter.Tests/JobInfoTests.cs
+++ Reporter.Tests/JobInfoTests.cs
@@ -1,5 +1,5 @@
 ﻿using System.Globalization;
-using Kontur.TestAnalytics.Reporter.Client;
+using Kontur.TestAnalytics.Reporter.Cli;
 using NUnit.Framework;
 
 namespace Kontur.TestAnalytics.Reporter.Tests;
@@ -7,43 +7,31 @@ namespace Kontur.TestAnalytics.Reporter.Tests;
 public class JobInfoTests
 {
     [Test]
-    public async Task METHOD()
+    public void RemoveDuplicatePartInClassNameTests()
     {
-        var startedAtFromJobEnv = "2024-10-30T10:42:19+03:00"; // тут время уже в указаном поясе (то есть по UTC = 05:42:19)
-        
-        var timestampFromKewebJunit = "2024-10-29T14:17:11"; // совпадает с временем ОКОНЧАНИЯ теста в логах джобы (UTC)
-        var timestampFromAllFormsTests = "2024-10-29T13:44:54"; // совпадает с временем начала теста в логах джобы (UTC)
-        
-        var start = DateTime.Parse("2024-10-30T12:59:21");
-        var end = DateTime.Parse("2024-10-30T13:34:32");
-        var duration = (long)(end - start).TotalMilliseconds;
-        
-        var utcOffset = TimeZoneInfo.Local.GetUtcOffset(DateTime.Now);
-        var commitMessage = $"wip start = {start.ToUniversalTime()}, end = {end}, utcOffset = {utcOffset}";
-
-        await TestRunsUploader.JobInfoUploadAsync(new FullJobInfo
+        var data = new List<(string className, string testcaseName, string expected)>
         {
-            JobId = "LOCAL_RUN_FOR_TEST_JOB",
-            JobRunId = "1",
-            BranchName = "milkov-test-local-run",
-            AgentName = "KE-FRM-AGENT-01",
-            AgentOSName = "Windows",
-            JobUrl = "https://kontur.ru",
-            TotalTestsCount = 101,
-            SuccessTestsCount = 99,
-            FailedTestsCount = 1,
-            SkippedTestsCount = 1,
-            State = JobStatus.Timeouted,
-            Duration = duration,
-            StartDateTime = start,
-            EndDateTime = end,
-            Triggered = "milkov.de@skbkontur.ru",
-            PipelineSource = "push",
-            CommitSha = "sdf4354sdf4354sdf4354sdf4354",
-            CommitMessage = commitMessage,
-            CommitAuthor = "milkov <milkov@skbkontur.ru>",
-            ProjectId = "12345",
-            CustomStatusMessage = "Hello world!",
-        });
+            (
+                "Kontur.Forms.Candy.Api.IntegrationTests.ApiControllerTests.GetDigestTests.GetDigestTests", 
+                "GetDigestTests.GetDigest_FindExactBik",
+                "Kontur.Forms.Candy.Api.IntegrationTests.ApiControllerTests.GetDigestTests."
+            ),
+            (
+                "Kontur.Forms.Candy.Api.IntegrationTests.ApiControllerTests.InnerController.ResolvePathTests",
+                "ResolvePathTests.ResolveInnerPath_Returns_ResolvedPath(\"/Файл/1/Документ/1/НДФЛ6.2/1/ОбязНА\",\"Файл/Документ/НДФЛ6.2/ОбязНА\",113103,\"fuf.xml\")",
+                "Kontur.Forms.Candy.Api.IntegrationTests.ApiControllerTests.InnerController."
+            ),
+            (
+                "Kontur.Forms.Api.FunctionalTests.DraftsApiV2CommonEditorFunctionalTest(Fns,\"warrant.xml\",\"windows-1251\",\"101415.xml\")",
+                "DraftsApiV2CommonEditorFunctionalTest(Fns,\"warrant.xml\",\"windows-1251\",\"101415.xml\").DraftWithAutoConvert_SetOldFuf_StartEditor_AllowConvert_EditorNewer",
+                "Kontur.Forms.Api.FunctionalTests."
+            )
+        };
+
+        foreach (var (className, testcaseName, expected) in data)
+        {
+            var classNameActual = JUnitReportHelper.RemoveDuplicatePartInClassName(className, testcaseName);
+            Assert.AreEqual(expected, classNameActual);
+        }
     }
 }
