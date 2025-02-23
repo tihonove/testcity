3eafa86bc64629238fbbe7f803901088dbc2ccbb
Eugene Tihonov <tihonov.ea@gmail.com>
2025-02-23 15:54:12 +0500

    Добавил юнит-тесты на фронте

---
+++ Front/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    reporters: [
+        "default",
+        ...(process.env.GITLAB_CI
+            ? [["jest-junit", { outputDirectory: "./.test-reports", outputName: "junit.xml" }]]
+            : []),
+    ],
+};
---
+++ Front/test/demo.test.ts
@@ -0,0 +1,8 @@
+describe("test", () => {
+    it("should work", () => {
+        expect(1).toBe(1);
+    });
+    it("should work 2", () => {
+        expect(1).toBe(2);
+    });
+});
