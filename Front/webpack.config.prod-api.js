eb40a00e396a9bd3ae0d99ac7824d6ef6d2a77cf
Eugene Tihonov <tihonov.ea@gmail.com>
2025-03-22 23:56:04 +0500

    chore: Fix webpack configuration for local development

--- Front/webpack.config.prod-api.js
+++ Front/webpack.config.prod-api.js
@@ -1,15 +1,16 @@
 /* eslint-disable */
-const config = require("./webpack.config");
-
-module.exports = {
-    ...config,
-    devServer: {
-        ...config.devServer,
-        proxy: config.devServer.proxy.map(x => ({ 
-            ...x, 
-            target: "https://testcity.kube.testkontur.ru", 
-            secure: false,
-            changeOrigin: true,
-        })),
-    }
-}
+module.exports = (env, argv) => {
+    const config = require("./webpack.config")(env, argv);
+    return ({
+        ...config,
+        devServer: {
+            ...config.devServer,
+            proxy: config.devServer.proxy.map(x => ({
+                ...x,
+                target: "https://testcity.kube.testkontur.ru",
+                secure: false,
+                changeOrigin: true,
+            })),
+        },
+    });
+};
