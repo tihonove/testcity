16d0915886d73d8df173f378e326fa8daff2c413
Eugene Tihonov <tihonov.ea@gmail.com>
2025-04-09 23:29:20 +0500

    chore: Fill commit parents tool #2

--- TestCity.Tool/Program.cs
+++ TestCity.Tool/Program.cs
@@ -12,7 +12,6 @@ Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
 var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
 var logger = loggerFactory.CreateLogger<Program>();
-logger.LogInformation("Logger initialized.");
 
 var clientProvider = new SkbKonturGitLabClientProvider(GitLabSettings.Default);
 var clientEx = clientProvider.GetExtendedClient();
@@ -21,10 +20,37 @@ var client = clientProvider.GetClient();
 var messageQueueClient = KafkaMessageQueueClient.CreateDefault(loggerFactory.CreateLogger<KafkaMessageQueueClient>());
 var workreClient = new WorkerClient(messageQueueClient);
 
+logger.LogInformation("Starting to process projects");
 foreach (var project in projects)
 {
-    await foreach (var commit in clientEx.GetAllRepositoryCommitsAsync(long.Parse(project.Id)).TakeWhile(x => x.CommittedDate > DateTime.Now.AddMonths(-2)))
+    logger.LogInformation("Processing project {ProjectId}: {ProjectTitle}", project.Id, project.Title);
+    int commitCount = 0;
+
+    try
     {
-        await workreClient.Enqueue(new BuildCommitParentsTaskPayload { ProjectId = long.Parse(project.Id), CommitSha = commit.Id });
+        await foreach (var commit in clientEx.GetAllRepositoryCommitsAsync(long.Parse(project.Id)).TakeWhile(x => x.CommittedDate > DateTime.Now.AddMonths(-2)))
+        {
+            logger.LogDebug("Processing commit {CommitSha} from {CommitDate} by {AuthorName}",
+                commit.Id, commit.CommittedDate, commit.AuthorName);
+
+            try
+            {
+                await workreClient.Enqueue(new BuildCommitParentsTaskPayload { ProjectId = long.Parse(project.Id), CommitSha = commit.Id });
+                logger.LogDebug("Successfully enqueued task for commit {CommitSha}", commit.Id);
+                commitCount++;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to enqueue task for commit {CommitSha} in project {ProjectId}", commit.Id, project.Id);
+            }
+        }
+
+        logger.LogInformation("Processed {CommitCount} commits for project {ProjectId}", commitCount, project.Id);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "Error processing project {ProjectId}: {ProjectTitle}", project.Id, project.Title);
     }
 }
+
+logger.LogInformation("Processing completed for all projects");
