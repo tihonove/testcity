54b9310ae923347d540df760f37ef366a990859c
Eugene Tihonov <tihonov.ea@gmail.com>
2025-04-09 23:19:29 +0500

    chore: Fill commit parents tool

--- TestCity.Tool/Program.cs
+++ TestCity.Tool/Program.cs
@@ -2,18 +2,29 @@
 using dotenv.net;
 using Kontur.TestCity.Core;
 using Kontur.TestCity.Core.GitLab;
+using Kontur.TestCity.Core.KafkaMessageQueue;
+using Kontur.TestCity.Core.Worker;
+using Kontur.TestCity.Core.Worker.TaskPayloads;
+using Microsoft.Extensions.Logging;
 
 DotEnv.Fluent().WithProbeForEnv(10).Load();
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
+var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+var logger = loggerFactory.CreateLogger<Program>();
+logger.LogInformation("Logger initialized.");
+
 var clientProvider = new SkbKonturGitLabClientProvider(GitLabSettings.Default);
 var clientEx = clientProvider.GetExtendedClient();
 var projects = GitLabProjectsService.GetAllProjects();
-foreach(var project in projects)
+var client = clientProvider.GetClient();
+var messageQueueClient = KafkaMessageQueueClient.CreateDefault(loggerFactory.CreateLogger<KafkaMessageQueueClient>());
+var workreClient = new WorkerClient(messageQueueClient);
+
+foreach (var project in projects)
 {
-    var jobs = await clientEx.GetAllProjectJobsAsync(long.Parse(project.Id)).Take(10).ToListAsync();
-    foreach (var job in jobs)
+    await foreach (var commit in clientEx.GetAllRepositoryCommitsAsync(long.Parse(project.Id)).TakeWhile(x => x.CommittedDate > DateTime.Now.AddMonths(-2)))
     {
-        Console.WriteLine($"Job ID: {job.Id}, Name: {job.Name}, Status: {job.Status}");
+        await workreClient.Enqueue(new BuildCommitParentsTaskPayload { ProjectId = long.Parse(project.Id), CommitSha = commit.Id });
     }
 }
