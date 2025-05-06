010b31955ad3583e038ab5763a342e63c22e6929
Eugene Tihonov <tihonov.ea@gmail.com>
2025-05-06 12:17:33 +0500

    Revert "chore: Switch to under maintenance"
    This reverts commit 5f59912f0210e073030059fb88a71bfa479e6df9.

--- Front/src/index.tsx
+++ Front/src/index.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 
 import { reject } from "./Utils/TypeHelpers";
+import { AppBootstrap } from "./AppBootstrap";
 import "./CommonStyles/Theme.css";
 import "./CommonStyles/Fonts.css";
 import "./CommonStyles/BaseTypography.css";
 import "./CommonStyles/Reset.css";
 import "./CommonStyles/GlobalStyle.css";
-import { AppUnderMaintenance } from "./AppUnderMaintenance";
 
 const root = createRoot(document.getElementById("root") ?? reject("Not found #root element"));
 
-root.render(<AppUnderMaintenance />);
+root.render(<AppBootstrap />);
