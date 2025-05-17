6da9462f67fa0da24613c880341320bb378e7a8a
Eugene Tihonov <tihonov.ea@gmail.com>
2025-05-17 21:39:05 +0500

    chore: Try fix publish test results - 2

--- Front/jest.config.js
+++ Front/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     reporters: [
         "default",
-        ...(process.env.GITLAB_CI
+        ...((process.env.GITLAB_CI || process.env.GITHUB_ACTIONS)
             ? [["jest-junit", { outputDirectory: "./.test-reports", outputName: "junit.xml" }]]
             : []),
     ],
