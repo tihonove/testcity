1cd59ce21734cb780c47edf09bb59a4b6eda0b70
Eugene Tihonov <tihonov.ea@gmail.com>
2025-07-20 19:32:52 +0200

    feat: Update test history bar chart component to recharts

--- Front/src/Components/RunStatisticsChart/RunStatisticsChart.tsx
+++ Front/src/Components/RunStatisticsChart/RunStatisticsChart.tsx
@@ -1,15 +1,34 @@
 import * as React from "react";
-import { Button, Tooltip } from "@skbkontur/react-ui";
-import { formatDuration } from "./DurationUtils";
-import { useLayoutEffect, useRef } from "react";
 import styles from "./RunStatisticsChart.module.css";
+import { Tooltip, Bar, BarChart, Brush, CartesianGrid, Rectangle, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import { formatDuration } from "./DurationUtils";
 
 interface RunStatisticsChartProps {
     value: Array<[state: string, duration: number, startDate: string]>;
 }
 
+interface BarShapeProps {
+    payload?: { "0": string };
+    [key: string]: unknown;
+}
+
+interface TooltipProps {
+    label?: string | number;
+}
+
+function BarShape(props: BarShapeProps) {
+    if (props.payload?.["0"] === "Success") {
+        return <Rectangle {...props} fill="rgb(var(--inverse-color-base) / 0.3)" />;
+    } else {
+        return <Rectangle {...props} fill="rgb(from var(--failed-bg-color) r g b / 50%)" />;
+    }
+}
+
+function CustomTooltip(props: TooltipProps) {
+    return <div className={styles.tooltip}>{props.label}</div>;
+}
+
 export function RunStatisticsChart(props: RunStatisticsChartProps): React.JSX.Element {
-    const [scale, setScale] = React.useState(1);
     const maxVisibleDuration = React.useMemo(() => {
         const maxDuration = props.value.reduce((x, y) => (x > y[1] ? x : y[1]), 0);
         if (maxDuration == 0) return 100;
@@ -23,76 +42,33 @@ export function RunStatisticsChart(props: RunStatisticsChartProps): React.JSX.El
         return Math.ceil(maxDuration / (60 * 1000)) * (60 * 1000);
     }, [props.value]);
 
-    const durationLabelStep = maxVisibleDuration / 4;
-    const scrollContainer = useRef<HTMLDivElement>(null);
-
-    useLayoutEffect(() => {
-        if (scrollContainer.current != undefined) scrollContainer.current.scrollLeft = 100000;
-    }, []);
-
     return (
         <div className={styles.chartContainer}>
-            <div className={styles.scaleButtons}>
-                <Button
-                    onClick={() => {
-                        setScale(scale / 2);
-                    }}>
-                    -
-                </Button>
-                <Button
-                    onClick={() => {
-                        setScale(scale * 2);
-                    }}>
-                    +
-                </Button>
-            </div>
-            <div className={styles.gaugeLabels}>
-                <div style={{ top: 0 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 0 * durationLabelStep)}
-                </div>
-                <div style={{ top: 25 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 1 * durationLabelStep)}
-                </div>
-                <div style={{ top: 50 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 2 * durationLabelStep)}
-                </div>
-                <div style={{ top: 75 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 3 * durationLabelStep)}
-                </div>
-                <div style={{ top: 100 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 4 * durationLabelStep)}
-                </div>
-            </div>
-            <div className={styles.gridLine} />
-            <div className={styles.gridLine} />
-            <div className={styles.gridLine} />
-            <div className={styles.gridLine} />
-            <div className={styles.scrollContainer} ref={scrollContainer}>
-                <div className={styles.container}>
-                    {reverse(props.value).map((x, index) => (
-                        <Tooltip trigger={"hover"} render={() => x[2]} key={index}>
-                            <div
-                                data-state={x[0] === "Success" ? "Success" : "Failed"}
-                                key={index}
-                                style={{
-                                    height: `${(100 * (x[1] / maxVisibleDuration)).toString()}px`,
-                                    flexBasis: `${(16 * scale).toString()}px`,
-                                }}>
-                                {index % (8 / scale) === 0 && <div className={styles.dateLabel}>{x[2]}</div>}
-                            </div>
-                        </Tooltip>
-                    ))}
-                </div>
-                <div className={styles.datesContainer}></div>
-            </div>
+            <ResponsiveContainer>
+                <BarChart data={props.value} width={500} height={200}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey={2} />
+                    <YAxis
+                        domain={[0, maxVisibleDuration]}
+                        dataKey={1}
+                        tickFormatter={(x: number) => {
+                            return formatDuration(maxVisibleDuration, x);
+                        }}
+                    />
+                    <Tooltip content={CustomTooltip} />
+                    <Brush
+                        fill={"var(--primary-bg)"}
+                        height={30}
+                        startIndex={Math.max(props.value.length - 200, 0)}
+                        endIndex={props.value.length - 1}>
+                        <BarChart>
+                            <XAxis dataKey={2} />
+                            <Bar dataKey={1} fill="rgb(var(--inverse-color-base) / 0.3)" shape={<BarShape />}></Bar>
+                        </BarChart>
+                    </Brush>
+                    <Bar dataKey={1} fill="rgb(var(--inverse-color-base) / 0.3)" shape={<BarShape />}></Bar>
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
-}
-
-function reverse<T>(items: T[]): T[] {
-    const result = [];
-    for (let i = 0; i < items.length; i++) {
-        result[items.length - 1 - i] = items[i];
-    }
-    return result;
 }
