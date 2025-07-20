329c6f5211f399b24e9ee12da07456e41ee39dcd
Eugene Tihonov <tihonov.ea@gmail.com>
2025-07-20 21:59:17 +0200

    wip

--- Front/src/Components/RunStatisticsChart/RunStatisticsChart.tsx
+++ Front/src/Components/RunStatisticsChart/RunStatisticsChart.tsx
@@ -3,6 +3,7 @@ import { Button, Tooltip } from "@skbkontur/react-ui";
 import { formatDuration } from "./DurationUtils";
 import { useLayoutEffect, useRef } from "react";
 import styles from "./RunStatisticsChart.module.css";
+import Draggable from "react-draggable";
 
 interface RunStatisticsChartProps {
     value: Array<[state: string, duration: number, startDate: string]>;
@@ -24,66 +25,116 @@ export function RunStatisticsChart(props: RunStatisticsChartProps): React.JSX.El
     }, [props.value]);
 
     const durationLabelStep = maxVisibleDuration / 4;
+    const brushLeft = useRef<HTMLDivElement>(null);
     const scrollContainer = useRef<HTMLDivElement>(null);
+    const container = useRef<HTMLDivElement>(null);
+    const brushContainer = useRef<HTMLDivElement>(null);
+
+    const druggingRef = React.useRef<boolean>(false);
 
     useLayoutEffect(() => {
         if (scrollContainer.current != undefined) scrollContainer.current.scrollLeft = 100000;
     }, []);
 
+    useLayoutEffect(() => {
+        console.log(111)
+    }, []);
+
+
     return (
-        <div className={styles.chartContainer}>
-            <div className={styles.scaleButtons}>
-                <Button
-                    onClick={() => {
-                        setScale(scale / 2);
-                    }}>
-                    -
-                </Button>
-                <Button
-                    onClick={() => {
-                        setScale(scale * 2);
-                    }}>
-                    +
-                </Button>
+        <div>
+            <div className={styles.chartContainer}>
+                <div className={styles.scaleButtons}>
+                    <Button
+                        onClick={() => {
+                            setScale(scale / 2);
+                        }}>
+                        -
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            setScale(scale * 2);
+                        }}>
+                        +
+                    </Button>
+                </div>
+                <div className={styles.gaugeLabels}>
+                    <div style={{ top: 0 }}>
+                        {formatDuration(maxVisibleDuration, maxVisibleDuration - 0 * durationLabelStep)}
+                    </div>
+                    <div style={{ top: 25 }}>
+                        {formatDuration(maxVisibleDuration, maxVisibleDuration - 1 * durationLabelStep)}
+                    </div>
+                    <div style={{ top: 50 }}>
+                        {formatDuration(maxVisibleDuration, maxVisibleDuration - 2 * durationLabelStep)}
+                    </div>
+                    <div style={{ top: 75 }}>
+                        {formatDuration(maxVisibleDuration, maxVisibleDuration - 3 * durationLabelStep)}
+                    </div>
+                    <div style={{ top: 100 }}>
+                        {formatDuration(maxVisibleDuration, maxVisibleDuration - 4 * durationLabelStep)}
+                    </div>
+                </div>
+                <div className={styles.gridLine} />
+                <div className={styles.gridLine} />
+                <div className={styles.gridLine} />
+                <div className={styles.gridLine} />
+                <div className={styles.scrollContainer} ref={scrollContainer}>
+                    <div className={styles.container} ref={container}>
+                        {reverse(props.value).map((x, index) => (
+                            <Tooltip trigger={"hover"} render={() => x[2]} key={index}>
+                                <div
+                                    data-state={x[0] === "Success" ? "Success" : "Failed"}
+                                    key={index}
+                                    style={{
+                                        height: `${(100 * (x[1] / maxVisibleDuration)).toString()}px`,
+                                        flexBasis: `${(16 * scale).toString()}px`,
+                                    }}>
+                                    {index % (8 / scale) === 0 && <div className={styles.dateLabel}>{x[2]}</div>}
+                                </div>
+                            </Tooltip>
+                        ))}
+                    </div>
+                    <div className={styles.datesContainer}></div>
+                </div>
             </div>
-            <div className={styles.gaugeLabels}>
-                <div style={{ top: 0 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 0 * durationLabelStep)}
-                </div>
-                <div style={{ top: 25 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 1 * durationLabelStep)}
-                </div>
-                <div style={{ top: 50 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 2 * durationLabelStep)}
-                </div>
-                <div style={{ top: 75 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 3 * durationLabelStep)}
-                </div>
-                <div style={{ top: 100 }}>
-                    {formatDuration(maxVisibleDuration, maxVisibleDuration - 4 * durationLabelStep)}
-                </div>
-            </div>
-            <div className={styles.gridLine} />
-            <div className={styles.gridLine} />
-            <div className={styles.gridLine} />
-            <div className={styles.gridLine} />
-            <div className={styles.scrollContainer} ref={scrollContainer}>
-                <div className={styles.container}>
-                    {reverse(props.value).map((x, index) => (
-                        <Tooltip trigger={"hover"} render={() => x[2]} key={index}>
-                            <div
-                                data-state={x[0] === "Success" ? "Success" : "Failed"}
-                                key={index}
-                                style={{
-                                    height: `${(100 * (x[1] / maxVisibleDuration)).toString()}px`,
-                                    flexBasis: `${(16 * scale).toString()}px`,
-                                }}>
-                                {index % (8 / scale) === 0 && <div className={styles.dateLabel}>{x[2]}</div>}
-                            </div>
-                        </Tooltip>
-                    ))}
-                </div>
-                <div className={styles.datesContainer}></div>
+            <div style={{ backgroundColor: "red", height: "100px" }} ref={brushContainer}>
+                <Draggable
+                    axis="x"
+                    bounds={{ left: 0, right: brushContainer.current?.clientWidth - 5 }}
+                    onStart={() => {
+                        druggingRef.current = true;
+                    }}
+                    onDrag={(e, { x, y }) => {
+                        // setBrushLeft(x);
+                        const s = scrollContainer.current;
+                        s.scrollLeft = x * (container.current?.scrollWidth / brushContainer.current?.clientWidth);
+                    }}
+                    onStop={(e, { x, y }) => {
+                        // setBrushLeft(x);
+                        druggingRef.current = false;
+                    }}>
+                    <div ref={brushLeft} style={{ position: "absolute", height: "100px", width: "5px", backgroundColor: "blue" }}></div>
+                </Draggable>
+                <Draggable
+                    axis="x"
+                    defaultPosition={{ x: 100, y: 0 }}
+                    bounds={{ left: 0, right: brushContainer.current?.clientWidth - 5 }}
+                    onStart={() => {
+                        druggingRef.current = true;
+                    }}
+                    onDrag={(e, { x, y }) => {
+                        // setBrushLeft(x);
+                        const s = scrollContainer.current;
+                        s.scrollLeft = x * (container.current?.scrollWidth / brushContainer.current?.clientWidth);
+                        brushLeft.current.style.transform = `translate(${x - 100}px, 0)`;
+                    }}
+                    onStop={(e, { x, y }) => {
+                        // setBrushLeft(x);
+                        druggingRef.current = false;
+                    }}>
+                    <div style={{ position: "absolute", height: "100px", width: "5px", backgroundColor: "blue" }}></div>
+                </Draggable>
             </div>
         </div>
     );
